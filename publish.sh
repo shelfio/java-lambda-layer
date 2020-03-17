16ef304c38db196c7949d9a15f82ce82cb965f88
Vitalii Sikora <vitalii.sikora@gmail.com>
2020-03-17 18:37:01 +0200

    refactor: publish lambda layer from S3

--- publish.sh
+++ publish.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-JAVA_VERSION=8
 LAYER_NAME='java'
 LAYER_VERSION=1
+BUCKET_NAME=shelf-java-lambda-layer-"$TARGET_REGION"
+
+aws s3 cp ./java.zip s3://"$BUCKET_NAME"/layer.zip
 
 LAYER_VERSION=$(
   aws lambda publish-layer-version --region "$TARGET_REGION" \
     --layer-name $LAYER_NAME \
-    --zip-file fileb://java.zip \
-    --description "Java v${JAVA_VERSION}" \
+    --content S3Bucket="$BUCKET_NAME",S3Key=layer.zip \
+    --description "Java 8" \
     --query Version \
     --output text
 )
